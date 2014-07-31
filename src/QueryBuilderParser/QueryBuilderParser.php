263d79d9b32fafdb70d7c4b4ab9eb2ab190c8127
Tim Groeneveld <tim@timg.ws>
2014-07-31 16:08:30 +1000

    QueryBuilder Parser.

---
+++ src/QueryBuilderParser/QueryBuilderParser.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace timgws;
+
+class QueryBuilderParser {
+
+    private $currentRule;
+
+    protected $operators = array (
+        'equal'            => array ('accept_values' => true,  'apply_to' => ['string', 'number', 'datetime']),
+        'not_equal'        => array ('accept_values' => true,  'apply_to' => ['string', 'number', 'datetime']),
+        'in'               => array ('accept_values' => true,  'apply_to' => ['string', 'number', 'datetime']),
+        'not_in'           => array ('accept_values' => true,  'apply_to' => ['string', 'number', 'datetime']),
+        'less'             => array ('accept_values' => true,  'apply_to' => ['number', 'datetime']),
+        'less_or_equal'    => array ('accept_values' => true,  'apply_to' => ['number', 'datetime']),
+        'greater'          => array ('accept_values' => true,  'apply_to' => ['number', 'datetime']),
+        'greater_or_equal' => array ('accept_values' => true,  'apply_to' => ['number', 'datetime']),
+        'begins_with'      => array ('accept_values' => true,  'apply_to' => ['string']),
+        'not_begins_with'  => array ('accept_values' => true,  'apply_to' => ['string']),
+        'contains'         => array ('accept_values' => true,  'apply_to' => ['string']),
+        'not_contains'     => array ('accept_values' => true,  'apply_to' => ['string']),
+        'ends_with'        => array ('accept_values' => true,  'apply_to' => ['string']),
+        'not_ends_with'    => array ('accept_values' => true,  'apply_to' => ['string']),
+        'is_empty'         => array ('accept_values' => false, 'apply_to' => ['string']),
+        'is_not_empty'     => array ('accept_values' => false, 'apply_to' => ['string']),
+        'is_null'          => array ('accept_values' => false, 'apply_to' => ['string', 'number', 'datetime']),
+        'is_not_null'      => array ('accept_values' => false, 'apply_to' => ['string', 'number', 'datetime'])
+    );
+
+    protected $operator_sql = array (
+        'equal'            => array ('operator' => '='),
+        'not_equal'        => array ('operator' => '!='),
+        'in'               => array ('operator' => 'IN'),
+        'not_in'           => array ('operator' => 'NOT IN'),
+        'less'             => array ('operator' => '<'),
+        'less_or_equal'    => array ('operator' => '<='),
+        'greater'          => array ('operator' => '>'),
+        'greater_or_equal' => array ('operator' => '>='),
+        'begins_with'      => array ('operator' => 'LIKE',     'append'  => '%'),
+        'not_begins_with'  => array ('operator' => 'NOT LIKE', 'append'  => '%'),
+        'contains'         => array ('operator' => 'LIKE',     'append'  => '%', 'prepend' => '%'),
+        'not_contains'     => array ('operator' => 'NOT LIKE', 'append'  => '%', 'prepend' => '%'),
+        'ends_with'        => array ('operator' => 'LIKE',     'prepend' => '%'),
+        'not_ends_with'    => array ('operator' => 'NOT LIKE', 'prepend' => '%'),
+        'is_empty'         => array ('operator' => '='),
+        'is_not_empty'     => array ('operator' => '!='),
+        'is_null'          => array ('operator' => 'NULL'),
+        'is_not_null'      => array ('operator' => 'NOT NULL')
+    );
+
+    private $fields;
+
+    public function QueryBuilderParser(array $fields = null)
+    {
+        $this->fields = $fields;
+    }
+
+    public function parse($json, \Illuminate\Database\Query\Builder $qb)
+    {
+        $query = @json_decode($json);
+
+        if ($error_number = json_last_error()) {
+            throw new QBParseException('JSON parsing threw an error: ' . json_last_error_msg());
+        }
+
+        if (!is_object($query)) {
+            throw new QBParseException('The query is not valid JSON');
+        }
+
+        if (!is_array($query->rules)) {
+            //throw new QBParseException('The query has no rules.');
+            return $qb;
+        }
+
+        return $this->convertQBrules($query->rules, $qb);
+    }
+
+    private function convertQBrules($rules, $qb, $nested = false)
+    {
+        // nested = true will add a nested where statement to the query.
+        foreach($rules as $rule) {
+            $this->currentRule = $rule;
+
+            if (isset($rule->rules)) {
+                $qb = $qb->whereNested(function($query) use (&$rule, &$qb) {
+                    foreach($rule->rules as $_rule) {
+                        $qb = $this->makeQuery($query, $_rule);
+                    }
+                }, $rule->condition);
+            } else {
+                $qb = $this->makeQuery($qb, $rule);
+            }
+        }
+
+        return $qb;
+    }
+
+    private function makeQuery($query, $rule)
+    {
+        print_r($rule);
+        if (!isset($rule->operator) or !isset($rule->id) or !isset($rule->field))
+            return $query;
+
+        if (!isset($rule->input) or !isset($rule->type))
+            return $query;
+
+        if (!isset($this->operators[$rule->operator]))
+            return $query;
+
+        $value = $rule->value;
+        $_sql_op = $this->operator_sql[$rule->operator];
+
+        if (isset($_sql_op['append']))
+            $value = $_sql_op['append'] . $value;
+
+        if (isset($_sql_op['prepend']))
+            $value =  $value . $_sql_op['prepend'];
+
+        if ($rule->operator == "is_null" or $rule->operator == "is_not_null")
+            $value = null;
+
+        if (is_array($this->fields) && !in_array($rule->field, $this->fields)) {
+            throw new QBParseException("Field ({$rule->field}) does not exist in fields list");
+        }
+
+        $query = $query->where($rule->field, $_sql_op['operator'], $value);
+
+        return $query;
+    }
+}
+
+class QBParseException extends \Exception
+{
+    
+}

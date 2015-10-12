475a04e5588d59c723f586fea164c65afc09fcb6
Tim Groeneveld <tim@timg.ws>
2015-10-12 12:04:50 +1100

    Traits are awesome.

---
+++ src/QueryBuilderParser/QBPFunctions.php
@@ -0,0 +1,81 @@
+<?php
+namespace timgws;
+
+trait QBPFunctions
+{
+    protected $operators = array (
+        'equal'            => array ('accept_values' => true,  'apply_to' => ['string', 'number', 'datetime']),
+        'not_equal'        => array ('accept_values' => true,  'apply_to' => ['string', 'number', 'datetime']),
+        'in'               => array ('accept_values' => true,  'apply_to' => ['string', 'number', 'datetime']),
+        'not_in'           => array ('accept_values' => true,  'apply_to' => ['string', 'number', 'datetime']),
+        'less'             => array ('accept_values' => true,  'apply_to' => ['number', 'datetime']),
+        'less_or_equal'    => array ('accept_values' => true,  'apply_to' => ['number', 'datetime']),
+        'greater'          => array ('accept_values' => true,  'apply_to' => ['number', 'datetime']),
+        'greater_or_equal' => array ('accept_values' => true,  'apply_to' => ['number', 'datetime']),
+        'between'          => array ('accept_values' => true,  'apply_to' => ['number', 'datetime']),
+        'begins_with'      => array ('accept_values' => true,  'apply_to' => ['string']),
+        'not_begins_with'  => array ('accept_values' => true,  'apply_to' => ['string']),
+        'contains'         => array ('accept_values' => true,  'apply_to' => ['string']),
+        'not_contains'     => array ('accept_values' => true,  'apply_to' => ['string']),
+        'ends_with'        => array ('accept_values' => true,  'apply_to' => ['string']),
+        'not_ends_with'    => array ('accept_values' => true,  'apply_to' => ['string']),
+        'is_empty'         => array ('accept_values' => false, 'apply_to' => ['string']),
+        'is_not_empty'     => array ('accept_values' => false, 'apply_to' => ['string']),
+        'is_null'          => array ('accept_values' => false, 'apply_to' => ['string', 'number', 'datetime']),
+        'is_not_null'      => array ('accept_values' => false, 'apply_to' => ['string', 'number', 'datetime'])
+    );
+
+    protected $operator_sql = array (
+        'equal'            => array ('operator' => '='),
+        'not_equal'        => array ('operator' => '!='),
+        'in'               => array ('operator' => 'IN'),
+        'not_in'           => array ('operator' => 'NOT IN'),
+        'less'             => array ('operator' => '<'),
+        'less_or_equal'    => array ('operator' => '<='),
+        'greater'          => array ('operator' => '>'),
+        'greater_or_equal' => array ('operator' => '>='),
+        'between'          => array ('operator' => 'BETWEEN'),
+        'begins_with'      => array ('operator' => 'LIKE',     'append'  => '%'),
+        'not_begins_with'  => array ('operator' => 'NOT LIKE', 'append'  => '%'),
+        'contains'         => array ('operator' => 'LIKE',     'append'  => '%', 'prepend' => '%'),
+        'not_contains'     => array ('operator' => 'NOT LIKE', 'append'  => '%', 'prepend' => '%'),
+        'ends_with'        => array ('operator' => 'LIKE',     'prepend' => '%'),
+        'not_ends_with'    => array ('operator' => 'NOT LIKE', 'prepend' => '%'),
+        'is_empty'         => array ('operator' => '='),
+        'is_not_empty'     => array ('operator' => '!='),
+        'is_null'          => array ('operator' => 'NULL'),
+        'is_not_null'      => array ('operator' => 'NOT NULL')
+    );
+
+    protected $needs_array = array(
+        'IN', 'NOT IN', 'BETWEEN',
+    );
+
+    /**
+     * Determine if an operator (LIKE/IN) requires an array.
+     *
+     * @param $operator
+     *
+     * @return bool
+     */
+    protected function operatorRequiresArray($operator)
+    {
+        return in_array($operator, $this->needs_array);
+    }
+
+    /**
+     * Make sure that a condition is either 'or' or 'and'.
+     *
+     * @param $condition
+     * @return string
+     * @throws QBParseException
+     */
+    protected function validateCondition($condition)
+    {
+        $condition = trim(strtolower($condition));
+        if ($condition !== 'and' && $condition !== 'or')
+            throw new QBParseException("Condition can only be one of: 'and', 'or'.");
+
+        return $condition;
+    }
+}
